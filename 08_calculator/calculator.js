2fe4e8938ab353b81d3301195de63e5f3c850874
Arkanheal <27277903+Arkanheal@users.noreply.github.com>
2023-06-24 21:13:39 +0200

    Solves exercices 8 and 9

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,29 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  return arr.reduce((acc, current) => add(acc, current), 0);
 };
 
-const multiply = function() {
-
+const multiply = function(...numbers) {
+  return numbers.reduce((acc, current) => acc *= current, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  if (a === 0) {
+    return 1
+  }
+  const arr = [...Array(a).keys()].map((i) => i += 1);
+  return multiply(...arr);
 };
 
 // Do not edit below this line
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (a) {
+  let str = a.trim().toLowerCase();
+  str = Array.from(str).filter((c) => {
+    const code = c.charCodeAt(0);
+    return (code > 47 && code < 58) ||(code > 96 && code < 123)
+  });
+  const strReversed = str.slice().reverse();
+  for (let i = 0; i < str.length; ++i) {
+    if (str[i] !== strReversed[i]) return false;
+  }
+  return true;
 };
 
 // Do not edit below this line
