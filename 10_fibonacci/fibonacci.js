ea555cfa2b451fa4cf9562621e6eb2abe23299e7
Arkanheal <27277903+Arkanheal@users.noreply.github.com>
2023-06-25 16:53:23 +0200

    Add solution to the last 3 exercices

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,6 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+  if (a <= 0) return "OOPS";
+  return a < 3 ? 1 : fibonacci(a - 1) + fibonacci(a - 2);
 };
 
 // Do not edit below this line
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(persons) {
+  return persons.reduce((oldest, person) => {
+    let currDateBirth = person.yearOfBirth;
+    let currDateDeath = person.yearOfDeath ?? new Date().getFullYear();
+    let currAge = currDateDeath - currDateBirth;
 
+    let oldestDateBirth = oldest.yearOfBirth;
+    let oldestDateDeath = oldest.yearOfDeath ?? new Date().getFullYear();
+    let oldestAge = oldestDateDeath - oldestDateBirth;
+    if (currAge >= oldestAge){
+      oldest = person ;
+    }
+    return oldest;
+  }, {
+    yearOfBirth: new Date().getFullYear(),
+    yearOfDeath: new Date().getFullYear()
+  })
 };
 
 // Do not edit below this line
